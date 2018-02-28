f59faf14c0ba891f0a9f4ba899d24abb5bccbcf2
Anton Pirker <anton@ignaz.at>
2018-02-28 21:38:52 +0100

    Refactored version

--- pipinator/settings.py
+++ pipinator/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'easy_thumbnails',
-    'pipinator.website',
+    'website',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'pipinator.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'pipinator.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
--- setup.py
+++ setup.py
@@ -1,38 +1,34 @@
 #!/usr/bin/env python3
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-base_dir = os.path.dirname(__file__)
-
-with open(os.path.join(base_dir, 'README.rst')) as f:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
     long_description = f.read()
 
 
 setup(
-    version='0.1.4',
+    version='0.2.0',
     name='pipinator',
     description='Example Project to show how to package a Django project for pip and PyPI',
     long_description=long_description,
-    author='Anton Pirker',
-    author_email='anton@ignaz.at',
-    url='https://github.com/antonpirker/django-pip-project',
-    keywords=['django'],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'pipinator = pipinator.__main__:main']
+            'pipinator = pipinator.manage:main',
+        ],
     },
     install_requires=[
         'Django==2.0.2',
         'easy-thumbnails==2.5',
-        'ipdb',
     ],
     classifiers=[
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
-    ]
+    ],
+    author='Anton Pirker',
+    author_email='anton@ignaz.at',
+    url='https://github.com/antonpirker/django-pip-project',
+    keywords=['django'],
 )
